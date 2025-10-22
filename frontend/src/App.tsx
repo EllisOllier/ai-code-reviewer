d9e67cbc2b76e57b0fdbd0cacd3d42b754ea1425
EllisOllier <ellisollier1@gmail.com>
2025-10-22 13:10:29 +0100

    Added dummy layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,14 +11,50 @@ function App() {
     }, []);
 
     return (
-        <div style={{ textAlign: "center", marginTop: "4rem" }}>
-            <h1>AI Code Reviewer</h1>
-            <p>API Status: {apiStatus ? apiStatus : "Offline" }</p>
+        <div id="root-container">
+            <div id="header">
+                <h1>AI Code Reviewer</h1>
+                <p>
+                    API Status:{" "}
+                    {apiStatus ? (
+                        <span id="online">{apiStatus}</span>
+                    ) : (
+                        <span id="offline">Offline</span>
+                    )}
+                </p>
+            </div>
 
-            <div>
-              <h2>Code Editor</h2>
-              <textarea></textarea>
-              <button id="analyze-button">Analyze</button>
+            <div id="code-editor-container">
+                <div id="left-code-editor">
+                  <div id="log-output">
+                    <ul id="log-list">
+                      <li><p>Dummy1</p></li>
+                      <li><p>Dummy2</p></li>
+                      <li><p>Dummy3</p></li>
+                      <li><p>Dummy4</p></li>
+                    </ul>
+                  </div>
+                </div>
+                <div id="right-code-editor">
+                    <div id="utility-container">
+                        <button id="analyze-button" className="utility-button">
+                            Analyze
+                        </button>
+                        <button id="dummy1-button" className="utility-button">
+                            Dummy1
+                        </button>
+                        <button id="dummy2-button" className="utility-button">
+                            Dummy2
+                        </button>
+                        <button id="dummy3-button" className="utility-button">
+                            Dummy3
+                        </button>
+                        <button id="dummy4-button" className="utility-button">
+                            Dummy4
+                        </button>
+                    </div>
+                    <textarea id="code-editor"></textarea>
+                </div>
             </div>
         </div>
     );
